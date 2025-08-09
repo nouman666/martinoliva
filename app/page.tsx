213b8febcce9bb6b5f35221eed1b71d361b70490
nouman666 <39223975+nouman666@users.noreply.github.com>
2025-08-09 01:05:45 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -125,7 +125,7 @@ const [animation, setAnimation] = useState({
         <div 
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: `url('https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')`
+  backgroundImage: `url(/bespoke-portfoliofront.png)`
           }}
         >
           <div className="absolute inset-0 bg-black bg-opacity-40"></div>
