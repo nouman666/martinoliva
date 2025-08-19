288e26dbe156693e1a81dc0357f421b885c990e7
nouman666 <39223975+nouman666@users.noreply.github.com>
2025-08-19 16:06:39 +0100

    Update page.tsx

--- app/engagement-rings/page.tsx
+++ app/engagement-rings/page.tsx
@@ -130,7 +130,7 @@ const { addItem,count, open } = useCart();
               <div className="text-xl md:text-2xl font-bold tracking-wider">
                 <span className="text-black">MARTIN OLIVA</span>
               <div className="text-xs text-gray-600 tracking-[0.3em] font-light">
-  The Total Watch and<br />
+  The Total Watch and <br/>
   Jewellery Care Centre 
 </div>
               </div>
