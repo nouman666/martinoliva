05fe27521123cf451549f25d4ab8046dfbb203aa
nouman666 <39223975+nouman666@users.noreply.github.com>
2025-08-26 18:59:32 +0100

    Update page.tsx

--- app/jewellery/page.tsx
+++ app/jewellery/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { useState } from 'react'
-import { Mail, Phone, Facebook, Instagram, Search, User, ShoppingBag,ChevronDown  } from 'lucide-react'
+import { Mail, Phone, Facebook, Instagram, Search, User, ShoppingBag, ChevronDown } from 'lucide-react'
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { useCart } from '@/app/cart/page'
+import { useSearch } from '@/app/search/SearchContext'
 
 export default function JewelleryPage() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
